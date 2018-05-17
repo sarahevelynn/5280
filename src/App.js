a860a450d4b6d0fac60fa140570629b380285787
SarahEvelynn Johnson <sarahevelynn@SarahEvelynns-MacBook-Pro.local>
2018-05-17 16:12:16 -0600

    finished first page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 import Header from "./Header/Header.js";
-
+import Footer from "./Footer/Footer.js";
+import Homepage from "./Welcome/Index.js"
 class App extends Component {
   render() {
     return (
@@ -10,6 +11,8 @@ class App extends Component {
         <Router>
           <div className="App-content">
             <Header />
+            <Homepage />
+            <Footer />
           </div>
       </Router>
       </div>
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,11 +4,29 @@ import logo from "./logo.png";
 
 export default function Header() {
   return (
-    <Link to="/">
     <header id="header">
-    <img src= {logo} className="logo" alt="logo" />
-    <h2 id="tagline">Helping Startups Startup</h2>
+    <div >
+      <Link to="/">
+        <img src={logo} className="logo" alt="logo" />
+      </Link>
+      </div >
+      <div id="navBar">
+        <h2 id="tagline">Helping Startups Startup</h2>
+        <nav>
+          <Link to="/">
+            <h2 id="navItem"> Home </h2>
+          </Link>
+          <Link to="/about">
+            <h2 id="navItem"> About Us </h2>
+          </Link>
+          <Link to="/services">
+            <h2 id="navItem"> Our Services </h2>
+          </Link>
+          <Link to="/contact">
+            <h2 id="navItem"> Contact Us </h2>
+          </Link>
+        </nav>
+      </div>
     </header>
-    </Link>
   );
 }
