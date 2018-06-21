49c6052c8d943cb22b82e1add14221ae4546f9a0
SarahEvelynn Johnson <sarah@5280startupsolutions.co>
2018-06-21 15:46:17 -0600

    formatting footer

--- src/srcDesktop/Footer/Footer.js
+++ src/srcDesktop/Footer/Footer.js
@@ -8,9 +8,18 @@ export default function Footer() {
     <footer>
       <p id="copy">&copy; 2018, 5280 Startup Solutions, LLC</p>
       <div id="social">
-      <a href={"mailto:sarah@5280startupsolutions.co"} target="_blank"><img src={email} className="socialPhoto" alt="email us" /></a>
-      <a href={"https://twitter.com/5280startup"} target="_blank"><img src={twitter} className="socialPhoto" alt="twitter" /></a>
-      <a href={"https://www.facebook.com/5280StartupSolutions/"} target="_blank"><img src={facebook} className="socialPhoto" alt="facebook" /></a>
+        <a href={"mailto:sarah@5280startupsolutions.co"} target="_blank">
+          <img src={email} className="socialPhoto" alt="email us" />
+        </a>
+        <a href={"https://twitter.com/5280startup"} target="_blank">
+          <img src={twitter} className="socialPhoto" alt="twitter" />
+        </a>
+        <a
+          href={"https://www.facebook.com/5280StartupSolutions/"}
+          target="_blank"
+        >
+          <img src={facebook} className="socialPhoto" alt="facebook" />
+        </a>
       </div>
     </footer>
   );
